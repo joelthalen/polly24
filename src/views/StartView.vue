b6d87501cf1b02faeb0e997217449dc54f54f135
JoelFamily <joel.thalen03@gmail.com>
2025-12-04 14:33:05 +0100

    första redigering av startview

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,61 +1,35 @@
 <template>
-  <header>
-    <div v-bind:class="['hamburger', {'close': !hideNav}]" 
-         v-on:click="toggleNav">
-    </div>
-    <div class="logo">
-      <img src="/img/gangsterbob.jpg">
-      Polly polling tool 
-      <img src="../assets/logo.svg">
-    </div>
-  </header>
-  <ResponsiveNav v-bind:hideNav="hideNav">
-    <button v-on:click="switchLanguage">
-      {{ uiLabels.changeLanguage }}
-    </button>
-    <router-link to="/create/">
-      {{ uiLabels.createPoll }}
-    </router-link>
-    <a href="">
-      {{ uiLabels.about }}
-    </a>
-    <a href="">FAQ</a>
-  </ResponsiveNav>
-  <h1>{{ uiLabels["sales-pitch"] }}</h1>
-  <h2>{{ uiLabels.subHeading }}</h2>
-  <label>
-    Write poll id: 
-    <input type="text" v-model="newPollId">
-  </label>
-  <router-link v-bind:to="'/lobby/' + newPollId">
-    {{ uiLabels.participatePoll }}
-  </router-link>
+    <section id =starViewSection>
+        <div id="logo"> <!-- Här ska loggan finnas-->
+            Are you smarter than a connect 4th grader
+        </div>
+        <div> <!--Här finns språkknappen-->
+          <button id="languageButton" :on-click="switchLanguage"> </button>
+        </div>
+        <div> <!--Knappen för att hosta-->
+          <button id="hostButton" :on-click="hostGame"></button>
+        </div>
+        <div> <!--Knappen för att joina-->
+          <input type="text" id="roomCode" v-model="roomCode" placeholder="Room code"> 
+          <button id="joinButton" :on-click="joinGame"></button>
+        </div>
+    </section>
 </template>
 
 <script>
-import ResponsiveNav from '@/components/ResponsiveNav.vue';
-import io from 'socket.io-client';
-const socket = io("localhost:3000");
+  export default {
+    name: 'StartView',
+    compoments: {
 
-export default {
-  name: 'StartView',
-  components: {
-    ResponsiveNav
-  },
-  data: function () {
-    return {
-      uiLabels: {},
-      newPollId: "",
-      lang: localStorage.getItem( "lang") || "en",
-      hideNav: true
-    }
-  },
-  created: function () {
-    socket.on( "uiLabels", labels => this.uiLabels = labels );
-    socket.emit( "getUILabels", this.lang );
-  },
-  methods: {
-    switchLanguage: function() {
+    },
+    data: function () {
+      return {
+        roomCode: '',
+        lang: localStorage.getItem("lang") || "en"
+      }
+    },
+    methods: {
+      switchLanguage: function() {
       if (this.lang === "en") {
         this.lang = "sv"
       }
@@ -64,61 +38,36 @@ export default {
       }
       localStorage.setItem( "lang", this.lang );
       socket.emit( "getUILabels", this.lang );
-    },
-    toggleNav: function () {
-      this.hideNav = ! this.hideNav;
+      },
     }
   }
-}
-</script>
-<style scoped>
-  header {
-    background-color: gray;
-    width: 100%;
-    display: grid;
-    grid-template-columns: 2em auto;
-  }
-  .logo {
-    text-transform: uppercase;
-    letter-spacing: 0.25em;
-    font-size: 2.5rem;
-    color: white;
-    padding-top:0.2em;
-  }
-  .logo img {
-    height:2.5rem;
-    vertical-align: bottom;
-    margin-right: 0.5rem; 
-  }
-  .hamburger {
-    color:white;
-    width:1em;
-    display: flex;
-    align-items: center;
-    justify-content: left;
-    padding:0.5rem;
-    top:0;
-    left:0;
-    height: 2rem;
-    cursor: pointer;
-    font-size: 1.5rem;
-  }
 
-@media screen and (max-width:50em) {
-  .logo {
-    font-size: 5vw;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .hamburger::before {
-    content: "☰";
-  }
-  .close::before {
-    content: "✕";
-  }
-  .hide {
-    left:-12em;
-  }
+</script>
+
+
+<style scoped>
+#starViewSection{
+    
 }
+
+#logo {
+
+}
+
+hostButton {
+
+}
+
+languageButton {
+  
+}
+
+joinButton {
+
+}
+
 </style>
+
+
+
+
